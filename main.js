387f8e168f4b3d916caa34700772d99d86f3640d
Mellonny <mellcontadeestudos@gmail.com>
2025-06-01 18:07:30 -0300

    Último ajuste da função checkItem

---
+++ main.js
@@ -0,0 +1,66 @@
+const items = []
+
+function addItem() {
+    const itemName = document.querySelector("#item").value
+
+    const item = {
+        name: itemName ,
+        checked: false
+    }
+
+    items.push(item)
+
+    document.querySelector("#item").value = ""
+
+    showItemsList()
+}
+
+function showItemsList() {
+    const sectionList = document.querySelector(".list")
+    sectionList.textContent = ""
+
+    items.map((item, index) => {
+        sectionList.innerHTML += ` 
+            <div class="item">
+                    <div>
+                        <input type="checkbox" name="list" id="item-${index}">
+
+                        <div class="custom-checkbox" onclick="checkItem('${item.name}')">
+                            <img src="./assets/checked.svg" alt="checked">
+                        </div>
+                        <label for="item-${index}" onclick=checkItem('${item.name}')">${item.name}</label>
+                    </div>
+
+
+                    <button onclick="removeItem('${item.name}')">
+                        <img src="./assets/trash-icon.svg" alt="Transh incon">
+                    </button>
+            </div>
+        `
+    })
+}
+
+function removeItem(itemName) {
+    const itemIndex = items.findIndex((item) => item.name === itemName)
+    const divWarning = document.querySelector(".warning")
+
+    divWarning.classList.remove("hide-warning")
+
+    setTimeout(() => {
+        divWarning.classList.add("hide-warning")
+    }, 4000)
+
+    if (itemIndex !== -1) {
+        items.splice(itemIndex, 1)
+    }
+
+    showItemsList()
+}
+
+function checkItem(itemName) {
+    const item = items.find((item) => item.name === itemName)
+
+    item.checked = !item.checked
+
+    showItemsList()
+}
